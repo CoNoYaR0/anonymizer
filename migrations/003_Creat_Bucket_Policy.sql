484b2d64cc18fa5e69347a7b7755ea15ecf01422
CoNoYaR0 <152749633+CoNoYaR0@users.noreply.github.com>
2025-08-01 01:10:22 +0200

    Update 003_Creat_Bucket_Policy.sql

--- migrations/003_Creat_Bucket_Policy.sql
+++ migrations/003_Creat_Bucket_Policy.sql
@@ -1,3 +1,7 @@
 CREATE POLICY "Allow public uploads in cvs bucket"
 ON storage.objects FOR INSERT TO public
 WITH CHECK ( bucket_id = 'cvs' );
+
+CREATE POLICY "Allow public read access on cvs bucket"
+ON storage.objects FOR SELECT TO public
+USING ( bucket_id = 'cvs' );
