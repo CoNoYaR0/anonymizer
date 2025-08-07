69760f7580098bb1633d31686dbcd92d3d7867a5
CoNoYaR0 <khalil.lost@gmail.com>
2025-08-07 10:00:08 +0200

    restart

--- migrations/004_add_hash_column.sql
+++
@@ -1,8 +0,0 @@
--- Add a new column to store the SHA256 hash of the uploaded file
-ALTER TABLE public.extractions
-ADD COLUMN file_hash VARCHAR(64);
-
--- Add a unique constraint to the new column to prevent duplicate file entries.
--- An index is automatically created on the column.
-ALTER TABLE public.extractions
-ADD CONSTRAINT unique_file_hash UNIQUE (file_hash);
